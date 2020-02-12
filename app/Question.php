a14782bf29512b82080a1925bb0e550b3539f91e
robo.setnicka.jr <robert.setnicka.jr@iss-europe.eu>
2020-02-12 19:29:21 +0100

    DatabaseSeeder

--- app/Question.php
+++ app/Question.php
@@ -8,7 +8,7 @@ class Question extends Model
 {
     protected $fillable = ['title', 'body'];
     public function user() {
-        return $this->belongTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function setTitleAttribute($value) {
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 3)->create()->each(function($u) {
+            echo $u . "\n";
+            $u->questions()
+                ->saveMany(
+                    factory(App\Question::class, rand(3, 7))->make()
+                );
+        });
     }
 }
